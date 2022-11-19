659b4089fad29a03cc02a1dd991ed5cc3a6f5954
g-martin772 <g.martin@htlkrems.at>
2022-11-19 10:16:40 +0100

    Update git submodules on setup.py

--- scripts/Setup.py
+++ scripts/Setup.py
@@ -19,5 +19,8 @@ if (not Vulkan.CheckVulkanSDK()):
 # if (not Vulkan.CheckVulkanSDKDebugLibs()):
 #     print("Vulkan SDK debug libs not found.")
 
+print("\nUpdating submodules...")
+subprocess.call(["git", "submodule", "update", "--init", "--recursive"])
+
 print("Running premake...")
 subprocess.call(["vendor/premake/bin/premake5.exe", "vs2022"])
